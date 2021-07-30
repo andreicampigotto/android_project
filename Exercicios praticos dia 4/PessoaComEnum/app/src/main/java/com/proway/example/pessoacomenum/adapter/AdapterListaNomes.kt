6a5466955e3244e7d73553919b71eccfc0e954e5
andreicampigotto <andreicampigotto@gmail.com>
2021-07-30 17:27:35 -0300

    ultimo commit do dia

--- Exercicios praticos dia 4/PessoaComEnum/app/src/main/java/com/proway/example/pessoacomenum/adapter/AdapterListaNomes.kt
+++ Exercicios praticos dia 4/PessoaComEnum/app/src/main/java/com/proway/example/pessoacomenum/adapter/AdapterListaNomes.kt
@@ -1,29 +1,31 @@
 package com.proway.example.pessoacomenum.adapter
 
 import android.view.LayoutInflater
+import android.view.TextureView
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.proway.example.pessoacomenum.R
 
-class AdapterListaNomes : RecyclerView.Adapter<ViewHolderNomes>(){
+class AdapterListaNomes(val arrayDeNomes: ArrayList<String>) : RecyclerView.Adapter<ViewHolderNomes>(){
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderNomes {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_lista_nome, false)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_lista_nome, parent, false)
             return ViewHolderNomes(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolderNomes, position: Int) {
-        TODO("Not yet implemented")
+        holder.textViewNome.text = arrayDeNomes[position]
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return arrayDeNomes.size
     }
 
 
 }
 
 class ViewHolderNomes(itemView: View) : RecyclerView.ViewHolder(itemView){
-
+    val textViewNome: TextView = itemView.findViewById(R.id.nameTextView)
 }
--- Exercicios praticos dia 4/PessoaComEnum/app/src/main/java/com/proway/example/pessoacomenum/ui/PrimeiraListaActivity.kt
+++ Exercicios praticos dia 4/PessoaComEnum/app/src/main/java/com/proway/example/pessoacomenum/ui/PrimeiraListaActivity.kt
@@ -2,8 +2,10 @@ package com.proway.example.pessoacomenum.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.proway.example.pessoacomenum.R
+import com.proway.example.pessoacomenum.adapter.AdapterListaNomes
 
 class PrimeiraListaActivity : AppCompatActivity() {
 
@@ -13,10 +15,12 @@ class PrimeiraListaActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_primeira_lista)
 
+        val arrayNomes = arrayListOf("Andrei", "Joana", "Carolina", "João",
+            "Maria", "Matheus", "Botão", "Arthur", "Felipe", "Jonathan")
+
         recyclerViewNomes = findViewById(R.id.recyclerViewNomes)
-
-
-        //recyclerViewNomes.
+        recyclerViewNomes.adapter = AdapterListaNomes(arrayNomes)
+        recyclerViewNomes.layoutManager = LinearLayoutManager(this)
 
     }
 }
