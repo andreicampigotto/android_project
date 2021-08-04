c11aa48705511fff50a962091495a57e64f83d56
andreicampigotto <andreicampigotto@gmail.com>
2021-08-04 17:23:15 -0300

    Criando Tela login

---
+++ Exercicios Grandes /Meu App/app/src/main/java/com/proway/myapplication/Login_Activity.kt
@@ -0,0 +1,11 @@
+package com.proway.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Login_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
