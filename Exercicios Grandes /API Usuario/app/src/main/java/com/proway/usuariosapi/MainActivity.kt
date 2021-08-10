625ccdcf7db927ef54a3a95e30a4ad2faf0f27d9
andreicampigotto <andreicampigotto@gmail.com>
2021-08-10 09:12:27 -0300

    criada a classe produto

--- Exercicios Grandes /API Usuario/app/src/main/java/com/proway/usuariosapi/MainActivity.kt
+++ Exercicios Grandes /API Usuario/app/src/main/java/com/proway/usuariosapi/MainActivity.kt
@@ -2,8 +2,10 @@ package com.proway.usuariosapi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.proway.usuariosapi.model.Product
+import retrofit2.Callback
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), Callback<List<Product>>  {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
