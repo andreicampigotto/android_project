91e52960eeb3305069f0be9b9e5bebcb02cd0913
andreicampigotto <andreicampigotto@gmail.com>
2021-08-10 11:56:02 -0300

    ajustado alguns metodos dentro do adapter para usar o reciclerView, e ajustado alguns metodos na classe Main

--- Exercicios Grandes /API Usuario/app/src/main/java/com/proway/usuariosapi/MainActivity.kt
+++ Exercicios Grandes /API Usuario/app/src/main/java/com/proway/usuariosapi/MainActivity.kt
@@ -1,25 +1,50 @@
 package com.proway.usuariosapi
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.proway.usuariosapi.adapter.AdapterProducts
 import com.proway.usuariosapi.model.Product
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class MainActivity : AppCompatActivity(), Callback<List<Product>>  {
+
+    private lateinit var recyclerView: RecyclerView
+    private val adapter = AdapterProducts()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        loadComponents()
+    }
 
+    private fun loadComponents() {
+        recyclerView = findViewById(R.id.productsRecyclerView)
+        recyclerView.layoutManager = GridLayoutManager(this, 2)
+        recyclerView.adapter = adapter
+    }
+
+    private fun fetchProducts() {
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        fetchProducts()
     }
 
     override fun onResponse(call: Call<List<Product>>, response: Response<List<Product>>) {
-        TODO("Not yet implemented")
+        response.body()?.apply {
+            adapter.update(this)
+        }
     }
 
     override fun onFailure(call: Call<List<Product>>, t: Throwable) {
-        TODO("Not yet implemented")
     }
 }
--- Exercicios Grandes /API Usuario/app/src/main/java/com/proway/usuariosapi/adapter/AdapterProducts.kt
+++ Exercicios Grandes /API Usuario/app/src/main/java/com/proway/usuariosapi/adapter/AdapterProducts.kt
@@ -1,25 +1,57 @@
 package com.proway.usuariosapi.adapter
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.proway.usuariosapi.R
+import com.proway.usuariosapi.model.Product
 
-class AdapterProducts: RecyclerView.Adapter<ViewHolderItemProduct>() {
+
+class AdapterProducts : RecyclerView.Adapter<ViewHolderItemProduct>() {
+
+    private var listOfProducts = mutableListOf<Product>()
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderItemProduct {
-        TODO("Not yet implemented")
+        LayoutInflater.from(parent.context).inflate(R.layout.item_product, parent, false).apply {
+            return ViewHolderItemProduct(this)
+        }
     }
 
     override fun onBindViewHolder(holder: ViewHolderItemProduct, position: Int) {
-        TODO("Not yet implemented")
+        listOfProducts[position].apply {
+            holder.bind(this)
+        }
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+    override fun getItemCount(): Int = listOfProducts.size
+
+    fun update(newList: List<Product>) {
+        listOfProducts = mutableListOf()
+        listOfProducts.addAll(newList)
+        notifyDataSetChanged()
     }
-
-
 }
 
-class ViewHolderItemProduct(itemView: View): RecyclerView.ViewHolder(itemView){
+class ViewHolderItemProduct(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    private val productImageView: ImageView = itemView.findViewById(R.id.imageViewProduct)
+    private val titleTextView: TextView = itemView.findViewById(R.id.textViewTitle)
+    private val subtitleTextView: TextView = itemView.findViewById(R.id.textViewSubTitle)
+    private val priceTextView: TextView = itemView.findViewById(R.id.textViewPrice)
 
+    fun bind(product: Product) {
+        titleTextView.text = product.title
+        subtitleTextView.text = product.description
+
+        priceTextView.text = "R$ ${product.price}"
+
+        Glide.with(itemView.context)
+            .load(product.image)
+            .placeholder(R.drawable.ic_baseline_image_24)
+            .into(productImageView)
+
+    }
 }
