dabd15d8e8b92e526414e7997e6e355d4e536e94
andreicampigotto <andreicampigotto@gmail.com>
2021-08-11 16:23:26 -0300

    exemplo de fragment com adapter generico

--- Exercicios praticos dia 10/Generic Adapter/app/src/main/java/com/proway/genericadapter/MainActivity.kt
+++ Exercicios praticos dia 10/Generic Adapter/app/src/main/java/com/proway/genericadapter/MainActivity.kt
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import androidx.fragment.app.Fragment
-import com.proway.genericadapter.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
