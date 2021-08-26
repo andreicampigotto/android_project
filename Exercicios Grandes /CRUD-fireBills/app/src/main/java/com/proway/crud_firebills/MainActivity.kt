34579a22fd57b24554641cfc54713557b7c1f096
andreicampigotto <andreicampigotto@gmail.com>
2021-08-26 08:57:50 -0300

    iniciando o crud de pordutos

---
+++ Exercicios Grandes /CRUD-fireBills/app/src/main/java/com/proway/crud_firebills/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.proway.crud_firebills
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.proway.crud_firebills.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
