b861cd4f6c1ceca1b13a258514aa539bcb447b4c
andreicampigotto <andreicampigotto@gmail.com>
2021-08-27 09:37:49 -0300

    Criado Adapter e telas

--- Exercicios Grandes /CRUD-fireBills/app/src/main/java/com/proway/crud_firebills/view/CategoryCRUDFrangment.kt
+++ Exercicios Grandes /CRUD-fireBills/app/src/main/java/com/proway/crud_firebills/view/CategoryCRUDFrangment.kt
@@ -1,16 +1,13 @@
 package com.proway.crud_firebills.view
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.proway.crud_firebills.R
 import com.proway.crud_firebills.view_model.MainViewModel
 
-class CategoryCRUDFrangment:Fragment() {
+class CategoryCRUDFrangment: Fragment(R.layout.category_crud_fragment) {
 
     companion object{
         fun newInstance() = CategoryCRUDFrangment()
@@ -18,13 +15,6 @@ class CategoryCRUDFrangment:Fragment() {
 
     private lateinit var viewModel: ViewModel
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.category_crud_fragment, container, false)
-    }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
