a334a7cfc04d1bccce511bc8f03cb40db1e60dd9
andreicampigotto <andreicampigotto@gmail.com>
2021-09-01 09:55:44 -0300

    ajustado o layout

--- API com injecao de depentencia/app/src/main/java/com/proway/injecao_depencia_com_api/view/MainFragment.kt
+++ API com injecao de depentencia/app/src/main/java/com/proway/injecao_depencia_com_api/view/MainFragment.kt
@@ -3,6 +3,7 @@ package com.proway.injecao_depencia_com_api.view
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
+import androidx.lifecycle.ViewModelProvider
 import com.proway.injecao_depencia_com_api.databinding.MainFragmentBinding
 import com.proway.injecao_depencia_com_api.view_model.MainViewModel
 
@@ -14,11 +15,11 @@ class MainFragment : Fragment() {
 
     private lateinit var viewModel: MainViewModel
     private lateinit var binding: MainFragmentBinding
-    
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        binding= MainFragmentBinding.bind(view)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
     }
 
 }
