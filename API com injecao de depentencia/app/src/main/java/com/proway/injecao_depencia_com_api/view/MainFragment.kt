a2c1878822005e61ef605b9ec0ca5f2765f04d06
andreicampigotto <andreicampigotto@gmail.com>
2021-09-01 09:31:19 -0300

    ajustado o view e o fragment

--- API com injecao de depentencia/app/src/main/java/com/proway/injecao_depencia_com_api/view/MainFragment.kt
+++ API com injecao de depentencia/app/src/main/java/com/proway/injecao_depencia_com_api/view/MainFragment.kt
@@ -17,17 +17,10 @@ class MainFragment : Fragment() {
 
     private lateinit var viewModel: MainViewModel
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
-    }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
     }
 
 }
--- API com injecao de depentencia/app/src/main/java/com/proway/injecao_depencia_com_api/view_model/MainViewModel.kt
+++ API com injecao de depentencia/app/src/main/java/com/proway/injecao_depencia_com_api/view_model/MainViewModel.kt
@@ -1,7 +1,32 @@
 package com.proway.injecao_depencia_com_api.view_model
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.proway.injecao_depencia_com_api.model.User
+import com.proway.injecao_depencia_com_api.repository.GitUserRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    private val gitUserRepository: GitUserRepository
+) : ViewModel() {
+
+    private val _user = MutableLiveData<List<User>>()
+    val user: LiveData<List<User>> = _user
+
+    private val _error = MutableLiveData<String>()
+    val error: LiveData<String> = _error
+
+    fun gerUserList() {
+        gitUserRepository.getUsersList { list, error ->
+            list.apply {
+                _user.value = this
+            }
+            error.apply {
+                _error.value = this
+            }
+        }
+    }
 }
